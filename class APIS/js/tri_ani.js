dfd77dcfd4ca79e735407cef09657c4fb92e0c91
SOCOLOMBIAN <sophie7304@outlook.com>
2025-03-06 14:05:57 -0500

    created a main script

--- class APIS/js/tri_ani.js
+++ class APIS/js/tri_ani.js
@@ -61,8 +61,36 @@ console.log("go")
 requestAnimationFrame(animate);
 }
 
+let triangle_2 = {
+    x1: canvas.width / 2,
+    y1: canvas.height / 2,
+    x2: canvas.width / 2 + lineLength,
+    y2: canvas.height / 2,
+    x3: canvas.width / 2 + lineLength / 2,
+    y3: canvas.height / 2 - lineLength,
+    strokeColor: "#FFFFFF",
+    fillColor: "#156cc8",
+    lineWidth: 2,
+    xSpeed:-1, 
+    ySpeed:1,
+    xRef:100,
+    yRef:100,
+  };
 
+  setPoints(triangle_2);
+   display(triangle_2);
+   triangle_2.xRef+=triangle_2.xSpeed;
+   triangle_2.yRef+=triangle_2.ySpeed;
 
+  function animate() {
+    //repaint with a black rect..
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    setPoints(triangle_1);
+    display(triangle_1);
+    triangle_1.xRef+=triangle_1.xSpeed;
+    triangle_1.yRef+=triangle_1.ySpeed;
 
+    requestAnimationFrame(animate);
+  }
 }
 
