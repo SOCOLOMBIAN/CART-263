a4d50b97e4271b2ca549f4c6e9889d17eee77f90
SOCOLOMBIAN <sophie7304@outlook.com>
2025-03-06 15:01:41 -0500

    added video

--- class APIS/js/controlVideo.js
+++ class APIS/js/controlVideo.js
@@ -1,5 +1,9 @@
 window.onload = function () {
     console.log("go");
+
+let canvas = document.getElementById("videoCanvas");
+  let context = canvas.getContext("2d");
+
     let video = document.getElementById("video");
     let playButton = document.getElementById("play");
     let pauseButton = document.getElementById("pause");
@@ -15,4 +19,15 @@ window.onload = function () {
     //set video.loop to true by default... BUT could be a button :)
    
     video.loop = true;
+    /*** instead of using the video object we can use the canvas **/
+  requestAnimationFrame(run);
+  function run() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.save();
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+    context.fillStyle = "#FFFFFF";
+    context.restore();
+    context.fillRect(canvas.width/2+50, canvas.height/2,50,50);
+    requestAnimationFrame(run);
+  }
   };
