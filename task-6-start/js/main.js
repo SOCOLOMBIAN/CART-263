e378654f9d60ee95bec5e3a32d45e78299d935db
SOCOLOMBIAN <sophie7304@outlook.com>
2025-04-03 16:24:02 -0400

    continue the task 6

--- task-6-start/js/main.js
+++ task-6-start/js/main.js
@@ -2,24 +2,22 @@ window.onload = run;
 
 function run() {
   document.querySelector("#stepOneButton").addEventListener("click", fetchText);
+ 
 
-
- /****** PART A:: FETCH */  
- async function fetchText() {
-    console.log("in fetch");
-    try {
-      let response= await fetch('../files/rainbow.txt');
-      let raw_rainbow_text= await response.text();
-      console.log(raw_rainbow_text);
-
-      document.getElementById("#rainbow_text").textContent= raw_rainbow_text;
-      document.querySelector("#resetButton").addEventListener("click", resetPoem);
-      runPartB(raw_rainbow_text);
-    } catch (e) {
-     
-    }
-  }
-
+/****** PART A:: FETCH */  
+async function fetchText() {
+  console.log("in fetch");
+  let raw_rainbow_text = "";
+  try {
+   
+    let response= await fetch("files/rainbow.txt")
+    let raw_rainbow_text= await response.text();
+    console.log(rainbowText);
+    //document.querySelector("#resetButton").addEventListener("click", resetPoem);
+    runPartB(raw_rainbow_text);
+  } catch (e) {}
+}
+  
   /****** PART B:: TEXT PROCESSING  */
   function runPartB(originalRainBowText) {
     document
@@ -28,22 +26,59 @@ function run() {
 
    /* FILL IN HERE */
     function producePoem() {
-      console.log(originalRainBowText)
+      //console.log(originalRainBowText)
+
+      //getting the user input 
+      const value = document.getElementById("phrase").value;
+      console.log(phrase_as_array);
+      
+      // split for the array 1
+      let phrase_as_array= imputUser.split(/["".?!\n|]/);
+
+      //slip for the text
+
+      let rainbow_tokens= originalRainBowText.slipt(/["".?!\n|]/);
+
       //SR
       runPartC(rainbow_tokens, phrase_as_array);
 
     }
   }
 
-
-  /****** PART C:: POEM CREATION  */
+ /****** PART C:: POEM CREATION  */
   function runPartC(rainbow_words, seed_phrase_array) {
     console.log(rainbow_words);
     console.log(seed_phrase_array);
- 
+
+    // the poem sentence
+    let poem_sentence="";
+    
+    // iteration on the seed_phrase_array
+    for (let i= 0; i < seed_phrase_array.length; i++){
+       const currentWord= seed_phrase_array[i];// the word from the seed 
+
+    // current word
+    for (let j= 0; j < currentWord.length; j++ ){
+       const nextChar= currentWord[j];
+
+    for (let k= 0; k < rainbow_words.lenght; k++){
+        const rainbow= rainbow_words[k];
+    
+        if (rainbow.lenght > j && rainbow[j] === nextChar )
+        {
+          if (poem_sentence.length >0){
+            poem_sentence += "";
+          }
+          poem_sentence += rainbow;
+          break
+
+        }
+      }
+  }
+}
+    
     //to next stage
     runPartD(poem_sentence);
-  }
 
   
    /****** PART D:: VISUALIZE  */
@@ -56,6 +91,6 @@ function run() {
   /*** TO FILL IN */
   
   }
-} //window onload
-
-
+ //window onload
+}
+}
