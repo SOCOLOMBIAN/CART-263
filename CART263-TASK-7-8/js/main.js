01c2d43fc8d21af9875cd04b16a66bcdafec17a4
SOCOLOMBIAN <sophie7304@outlook.com>
2025-04-07 22:11:38 -0400

    added the part 2

--- CART263-TASK-7-8/js/main.js
+++ CART263-TASK-7-8/js/main.js
@@ -6,7 +6,6 @@ try{
     // data from the json file
     const response= await fetch('data/iris.json');
 
-
     // check the fetch 
     if(!response.ok)
     {
@@ -15,8 +14,40 @@ try{
 
     const json= await response.json();
     console.log(json);
-  } catch (error){
-    console.error(error.message);
+    
+// possible colors 
+let possibleColor= ["#5d3fd3","#a73fd3","#d33fb5","#d35d3f","#d3a73f"];
 
+ const irisesWithColors= json.map(item => {
+    const addRandomColor= possibleColor[Math.floor(Math.random()* possibleColor.length)];
+    return{
+        ...item,
+        color: addRandomColor
+    };
+  });
+ 
+ console.log(" colors",irisesWithColors);
+
+} catch (error){
+    console.error(error.message)
 }
+
+// filter(): Use the filter() on irisesWithColors to output a new array called 
+// filteredIrises. The task is to: 
+// filter out all objects whose `sepalWidth` >=4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
