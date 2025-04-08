a8ef11b63cb5d42d1f07511b051f2053e4934aca
SOCOLOMBIAN <sophie7304@outlook.com>
2025-04-07 21:23:07 -0400

    added part 1

--- CART263-TASK-7-8/js/main.js
+++ CART263-TASK-7-8/js/main.js
@@ -1,4 +1,22 @@
 window.onload = async function(){
     console.log("task 7-8");
 
+try{
+
+    // data from the json file
+    const response= await fetch('data/iris.json');
+
+
+    // check the fetch 
+    if(!response.ok)
+    {
+        throw new Error(`Response status: ${response.status}`);  
+    }
+
+    const json= await response.json();
+    console.log(json);
+  } catch (error){
+    console.error(error.message);
+
+}
 }
